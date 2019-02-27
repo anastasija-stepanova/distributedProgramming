502129a62ab69bb431fc5db0a9562956f7031739
nastyaaa19980 <nastyaaa19980@gmail.com>
2019-02-27 23:15:30 +0300

    Добавлена 3 лаб работа

---
+++ lab1/src/TextListener/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = RedisStore.RedisCache;
+            var sub = redis.Multiplexer.GetSubscriber();
+            sub.Subscribe("events", (channel, message) => {
+                Console.WriteLine("Id: " + (string)message);
+                Console.WriteLine("val: " + redis.StringGet((string)message));
+            });
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- lab1/v0.0.1/run.cmd
+++ lab1/v0.0.1/run.cmd
@@ -1,2 +1,3 @@
 start "backend v0.0.1" dotnet backend/backend.dll
 start "frontend v0.0.1" dotnet frontend/frontend.dll 
+start "textListener v0.0.1" dotnet textListener/textListener.dll
